28644a5236ab1b8909242566f5149a37b9183cae
abhay <i.am.abhaysalvi@gmail.com>
2023-03-09 18:53:38 +0530

    minor change

--- src/routes/Panel.svelte
+++ src/routes/Panel.svelte
@@ -5,12 +5,12 @@
   let elements: any = {
     Heading: {
       icon: '../src/Icons/heading.svg',
-      description: 'Add a big heading',
+      description: 'Big section heading',
       code: '<h1>Heading</h1>'
     },
     Text: {
       icon: '../src/Icons/text.svg',
-      description: 'New paragraph',
+      description: 'Plain paragraph',
       code: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.</p>'
     },
     Button: {
