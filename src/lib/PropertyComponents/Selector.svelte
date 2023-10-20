693a8e962f615c8ec283aa41cac8b3cc4d335867
abhay <i.am.abhaysalvi@gmail.com>
2023-10-20 17:43:13 +0530

    added tagify for adding tagged classes

--- src/lib/PropertyComponents/Selector.svelte
+++ src/lib/PropertyComponents/Selector.svelte
@@ -1,39 +1,67 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
   import Dropdown from '../UIComponents/Dropdown.svelte';
   import ButtonGroup from '../UIComponents/ButtonGroup.svelte';
   import Input from '$lib/UIComponents/Input.svelte';
-
-  let classInputOpen = true;
-  let idInputOpen = false;
+  import Tagify from '@yaireo/tagify';
+  import '@yaireo/tagify/dist/tagify.css';
 
   function toggleClassId(e: Event) {
     let clickedBtn = e.target as HTMLButtonElement;
-    if (clickedBtn.id === 'selector-class') {
-      classInputOpen = true;
-      idInputOpen = false;
-    } else if (clickedBtn.id === 'selector-id') {
-      classInputOpen = false;
-      idInputOpen = true;
+    let classInput = <HTMLInputElement>document.querySelector('.tagify.tag-input');
+    let idInput = <HTMLInputElement>document.querySelector('.id-input');
+
+    if (clickedBtn.id === 'selector-class-btn') {
+      classInput.classList.remove('hidden');
+      idInput.classList.add('hidden');
+    } else if (clickedBtn.id === 'selector-id-btn') {
+      idInput.classList.remove('hidden');
+      classInput.classList.add('hidden');
     }
   }
+
+  onMount(() => {
+    let classInput = <HTMLInputElement>document.querySelector('.tag-input');
+    let tagify = new Tagify(classInput);
+  });
 </script>
 
 <div class="flex flex-col px-[14px] py-[12px] gap-[10px]">
   <div class="flex justify-between gap-[8px]">
-    <!-- Button group -->
     <ButtonGroup
       ItemsArray={['Class', 'ID']}
-      IdArray={['selector-class', 'selector-id']}
+      IdArray={['selector-class-btn', 'selector-id-btn']}
       customFunction={toggleClassId}
     />
 
-    <!-- dropdown -->
     <Dropdown
       DropdownBtnText={'Set State'}
       ItemsArray={['Default', 'On Hover', 'On Press', 'On Focus']}
     />
   </div>
 
-  <Input show={classInputOpen} id={'class-input'} placeholder={'add a new class'} />
-  <Input show={idInputOpen} id={'id-input'} placeholder={'add a new ID'} />
+  <Input placeholder={'Add a new class'} Class={'tag-input'} />
+  <Input placeholder={'Add a new id'} Class={'id-input hidden'} />
 </div>
+
+<style>
+  :global(.tag-input) {
+    --tag-bg: #0070e7;
+    --tag-hover: #0070e7c1;
+    --tags-hover-border-color: #0070e7c1;
+    --tags-focus-border-color: #0070e7c1;
+    --tag-text-color: #ebe9e9;
+    --tag-remove-btn-color: #ebe9e9;
+    --placeholder-color: #b8b6b6a1;
+    --placeholder-color-focus: #b8b6b6a1;
+    --input-color: #b8b6b6;
+    --tag-hide-transition: 0s;
+    --loader-size: 0em;
+    --tag-text-color--edit: #b8b6b6;
+    --tag-inset-shadow-size: 20px;
+    --tag-pad: 4px;
+    border-color: #505050;
+    border-width: 2px;
+    border-radius: 5px;
+  }
+</style>
--- src/lib/UIComponents/Input.svelte
+++ src/lib/UIComponents/Input.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-  export let id: string;
+  export let id: string = '';
+  export let Class: string = '';
   export let show: boolean = true;
   export let placeholder: string = 'placeholder';
 </script>
@@ -7,8 +8,15 @@
 <input
   {id}
   class={show
-    ? 'w-full text-[11px] bg-[#404040] font-sans font-semibold placeholder-[#b8b6b6a1] text-[#b8b6b6] border-[2px] border-[#505050] rounded-[5px] px-[10px] py-[7px] outline-none'
+    ? 'w-full text-[11px] bg-[#404040] font-sans font-semibold placeholder-[#b8b6b6a1] text-[#b8b6b6] border-[2px] border-[#505050] rounded-[5px] px-[10px] py-[7px] outline-none ' +
+      Class
     : 'hidden'}
   {placeholder}
   spellcheck="false"
 />
+
+<style>
+  .hidden {
+    display: none;
+  }
+</style>
